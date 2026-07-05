f80a4f42bca048c9b8263ad78359b0cc148e5921
Olga Polyanskaya <polyanskaya3000@gmail.com>
2026-07-05 12:04:47 +0700

    test: add cart checks and screenshots

--- tests/shoppingCart.spec.ts
+++ tests/shoppingCart.spec.ts
@@ -1,9 +1,28 @@
 import {test, expect} from '@playwright/test';
 
+test('Пустая корзина', async ({page}) => {
+  await page.goto('cart');
+  await page.getByTestId('cookie-accept-button').click();
+  await expect.soft(page).toHaveScreenshot({
+    fullPage: true,
+  });
+})
+
 test('Корзина с товаром', async ({page}) => {
   await page.goto('catalog');
   await page.getByTestId('catalog-add-to-cart-button-prod-001').click();
   await page.getByTestId('catalog-add-to-cart-button-prod-001').click();
   await page.getByTestId('header-cart-button').click();
-  await expect.soft(page).toHaveScreenshot();
+  await page.getByTestId('cookie-accept-button').click();
+  await expect.soft(page).toHaveScreenshot({
+    fullPage: true,
+    mask: [
+      page.getByTestId('cart-total-price'),
+      page.getByTestId('cart-captcha-image') // хотелось бы еще добавить маскирование картинки товара, названия и цены, т.к. товар может быть другим
+    ]
+  });
 })
+
+
+
+
