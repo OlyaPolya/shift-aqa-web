9c44d89574cbf5456fef2da707f9cdd0ab20a925
Olga Polyanskaya <polyanskaya3000@gmail.com>
2026-07-06 14:54:22 +0700

    test: add mask to product image and info at cart checks

--- tests/shoppingCart.spec.ts
+++ tests/shoppingCart.spec.ts
@@ -1,24 +1,27 @@
 import {test, expect} from '@playwright/test';
 
-test('Пустая корзина', async ({page}) => {
-  await page.goto('cart');
-  await page.getByTestId('cookie-accept-button').click();
-  await expect.soft(page).toHaveScreenshot({
-    fullPage: true,
-  });
-})
-
 test('Корзина с товаром', async ({page}) => {
   await page.goto('catalog');
-  await page.getByTestId('catalog-add-to-cart-button-prod-001').click();
-  await page.getByTestId('catalog-add-to-cart-button-prod-001').click();
+
+  const addCartBtn = page.getByTestId('catalog-add-to-cart-button-prod-001');
+
+  await addCartBtn.click();
+  await addCartBtn.click();
   await page.getByTestId('header-cart-button').click();
   await page.getByTestId('cookie-accept-button').click();
+
+  const cardContent = page.getByTestId('cart-item-prod-001');
+  const productImage = cardContent.getByRole('img');
+  const productInfo = cardContent.locator('div', { hasText: '₽/кг' }) // решила объединить информацию и продукте в корзине в единый блок для маскирования, т.к. может измениться название, цена и категория, кроме текста '₽/кг'. Воспользовалась документацией playwright, т.к. не знала как на div, содержащий текст '₽/кг', выйти. Я подумала, что '₽/кг' - это наиболее стабильный параметр в нашем случае, нежели, классы и теги
+
   await expect.soft(page).toHaveScreenshot({
     fullPage: true,
     mask: [
       page.getByTestId('cart-total-price'),
-      page.getByTestId('cart-captcha-image') // хотелось бы еще добавить маскирование картинки товара, названия и цены, т.к. товар может быть другим
+      page.getByTestId('cart-captcha-image'),
+      page.locator('[data-testid^="catalog-product-price-prod-"]'),
+      productImage,
+      productInfo
     ]
   });
 })
